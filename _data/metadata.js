98f9b869c1f75824329d24aca57d35bb578f46c8
scario <davide.scarioni@gmail.com>
2024-11-10 19:03:51 +0100

    First commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Scario Side Quest",
+	url: "https://example.com/",
+	language: "it",
+	description: "Descrizione del blog",
+	author: {
+		name: "Davide Scarioni",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
+	}
+}
